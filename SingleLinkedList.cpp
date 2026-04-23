c5947132d805580181ada176f1dc117ba1911631
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-04-23 16:13:03 +0700

    menambahkan insert di awal linked list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -11,11 +11,53 @@ public:
 
 class LinkedList
 {
+private:
     Node *START;
 
 public:
     LinkedList()
     {
         START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+        nodeBaru->next = NULL;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim > current->noMhs))
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        if ((current != NULL) && (nim == current->noMhs))
+        {
+            cout << "\nDuplikasi tidak diijinkan\n";
+            return;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
     }
 };
